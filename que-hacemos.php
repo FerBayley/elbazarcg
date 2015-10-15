853f9f0e47e62eecd67e13792ec23f2ac7c6ee41
fernando.bayley@gmail.com <fernando.bayley@gmail.com>
2015-10-15 11:56:41 -0300

    Primer subida

---
+++ que-hacemos.php
@@ -0,0 +1,41 @@
+<!DOCTYPE html>
+<html lang="es-Ar">
+<head>
+	<meta charset="UTF-8">
+	<title>El Bazar CG</title>
+	<!-- METAVP -->
+	<meta name="viewport" content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+	<!-- METAVP -->	
+	<!-- CSS -->
+	<link rel="stylesheet" href="public/css/bootstrap.min.css" />
+	<link rel="stylesheet" href="public/css/main.css" />
+	<!-- CSS -->
+
+	<!--[if lt IE 9]>
+	<script src="http://html5shim.googlecode.com/svn/trunk/html5.js"></script>
+	<![endif]-->
+</head>
+<body>
+
+
+	<div class="Contenedor-logo">
+		<a href="index.php">
+			<img src="public/images/logo.png" width="191" height="191" class="Card-logo" alt="El Bazar Conceptos Gastronomicos">
+		</a>
+	</div> <!-- End of Contenedor-logo -->
+
+	<div class="Contenedor-texto">
+		<article>
+			<p>Estamos trabajando en el desarrollo de esta sección</p>
+			<p>Pronto estaremos on line, muchas gracias</p>
+		</article>
+	</div> <!-- End of Contenedor-texto -->
+
+	<div class="Pie">
+		<article>
+			<p>Contacto:<a href="mailto:Contacto:administracion@elbazarcg.com"> administracion@elbazarcg.com</a></p>
+		</article>
+	</div> <!-- End of pie -->
+	
+</body>
+</html>
